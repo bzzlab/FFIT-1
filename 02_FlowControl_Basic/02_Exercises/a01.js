7d11638c7693044fd85461e55ea51dd02a11007c
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-09-05 10:19:01 +0200

    02_ Exerc

--- 02_FlowControl_Basic/02_Exercises/a01.js
+++ 02_FlowControl_Basic/02_Exercises/a01.js
@@ -7,18 +7,18 @@ Aufgabe - Zusätzlich Temperatur zur Meldung ausgeben
  */
 //Input
 'use strict';
-let temp = 25;
+let temp = 36;
 //When temperature is lower equal 4 ...
 //??
     //It is .. °C and it's freezing outside!
 //??
-//Else when temperature is more equal 35 ...
-//??
-    //It is .. °C and  and very hot outside!
-//??
 //Else when temperature is lower equal 25 ...
 //??
     //It's .. °C and pretty nice out!
+//??
+// Else when temperature is more equal 35 ...
+//??
+    //It is .. °C and  and very hot outside!
 //??
 //Else
 //??
