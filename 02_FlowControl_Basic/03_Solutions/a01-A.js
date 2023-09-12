209ad8d3c4448ba9179a2e2fe8e1a8c680be185a
Daniel Garavaldi <daniel.garavaldi@bzz.ch>
2023-09-12 11:22:23 +0200

    02_* ,03_* sol

---
+++ 02_FlowControl_Basic/03_Solutions/a01-A.js
@@ -0,0 +1,29 @@
+/*
+Thema: if-then-else ("wenn-dann-sonst", conditional statements)
+    und switch-case
+ */
+/*
+Aufgabe - Zusätzlich Temperatur zur Meldung ausgeben
+ */
+//Input
+'use strict';
+let temp = 36;
+//When temperature is lower equal 4 ...
+if (temp <= 4){//??
+    //It is .. °C and it's freezing outside!
+    console.log(`It is ${temp}°C and it's freezing outside!`);//??
+//Else when temperature is lower equal 25 ...
+} else if (temp <= 25){//??
+    //It's .. °C and pretty nice out!
+    console.log(`It's ${temp}°C and pretty nice out!`)//??
+// Else when temperature is more equal 35 ...
+} else if (temp >= 35){//??
+    //It is .. °C and  and very hot outside!
+    console.log(`It's ${temp}°C and very hot outside!`);//??
+//Else
+} else {//??
+    //It's .. °C and still quite warm!
+    console.log(`It's ${temp}°C and still quite warm!`)//??
+}//??
+
+
